a83cf60d86f72fd649e64fe2e7bb94ed78b28fbc
Rohit Sinha <rohit@hashgraph.com>
2025-05-27 09:16:24 -0400

    feat: library method for pairing response message
    Signed-off-by: Rohit Sinha <rohit@hashgraph.com>

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -22,8 +22,38 @@ pub fn create_contact_message(
         public_key_id: channel_id,
         transport_uri: transport_uri.clone(),
         mlkem_encapsulation_key: pk.mlkem_encapsulation_key,
-        ecies_public_key: Vec::new(),
+        ecies_public_key: pk.ecies_public_key,
         nonce: rng.next_u64(),
         message_encoding_type: 0,
+    }
+}
+
+pub fn produce_pairing_request_message(
+    channel_id: u64,
+    kind: derec_proto::SenderKind,
+    contact_message: &derec_proto::ContactMessage
+) -> derec_proto::PairRequestMessage {
+    // extract the PairingContactMessageMaterial from the contact message
+    let pk = pairing::PairingContactMessageMaterial {
+        mlkem_encapsulation_key: contact_message.mlkem_encapsulation_key.clone(),
+        ecies_public_key: contact_message.ecies_public_key.clone(),
+    };
+
+    let mut rng = rand::rngs::OsRng;
+
+    // generate the public key material
+    let mut seed = [0u8; 32];
+    rng.fill_bytes(&mut seed);
+    let (pk, _sk) = pairing::pairing_request_message(seed, &pk)
+        .expect("Failed to generate pairing request message");
+
+    derec_proto::PairRequestMessage {
+        sender_kind: kind.into(),
+        mlkem_ciphertext: pk.mlkem_ciphertext,
+        ecies_public_key: pk.ecies_public_key,
+        public_key_id: channel_id,
+        nonce: contact_message.nonce,
+        communication_info: None,
+        parameter_range: None,
     }
 }
