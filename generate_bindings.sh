a1ad177ca9604b20c422ef4f38a2c8e3cc32c732
Rohit Sinha <sinharo@gmail.com>
2025-07-03 10:55:47 -0400

    feat: js bindings
    Signed-off-by: Rohit Sinha <sinharo@gmail.com>

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -1,2 +1,4 @@
-mkdir -p bindings/swift
-cargo r --features=uniffi/cli --bin uniffi-bindgen -- generate --language swift --library target/release/libderec_library.dylib --out-dir bindings/swift
+mkdir -p library/bindings/swift
+cargo r --features=uniffi/cli --bin uniffi-bindgen -- generate --language swift --library target/release/libderec_library.dylib --out-dir library/bindings/swift
+
+bunx wasm-pack build --out-dir bindings/typescript --mode normal --release --target bundler library --no-default-features --features ffi_wasm
